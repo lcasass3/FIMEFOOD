4c180f8cdc7fb9175f8f1ce8682505ad6b3a93e4
Leon Casas <leonn.casass@gmail.com>
2023-10-12 10:00:44 -0600

    Se restructuran las rutas
    - Se agregan las paginas Home, Login y Landing y se incorporan al router
    - Se elimina TestPage

--- src/pages/Landing/LandingPage.vue
+++ src/pages/Landing/LandingPage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hola</h1>
+  <div>
+    <h1>Landing</h1>
+  </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestPage from '../pages/TestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'test',
-      component: TestPage
+      name: 'landing',
+      component: () => import('../pages/Landing/LandingPage.vue')
     },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../pages/Home/HomePage.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/Login/LoginPage.vue')
+    }
   ]
 })
 
