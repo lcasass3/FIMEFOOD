87d6bbb47ab97722c779f4e618e65a8fedb776bf
Leon Casas <leonn.casass@gmail.com>
2023-10-12 16:16:17 -0600

    work in progress

---
+++ src/components/VButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  type: 'primary' | 'secondary' | 'danger' | 'outline'
+}>()
+</script>
+
+<template>
+  <button
+    class="btn"
+    type="button"
+    :class="{
+      'btn-primary': type === 'primary',
+      'btn-secondary': type === 'secondary',
+      'btn-danger': type === 'danger',
+      'btn-outline-primary': type === 'outline'
+    }"
+  >
+    <slot></slot>
+  </button>
+</template>
--- src/pages/Landing/LandingPage.vue
+++ src/pages/Landing/LandingPage.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBarComponent from './components/NavBarComponent.vue'
+import VButton from '@/components/VButton.vue'
+</script>
 
 <template>
-  <div>
-    <h1>Landing</h1>
-  </div>
+  <NavBarComponent />
+
+  <main class="container h-100">
+    <div class="row h-100">
+      <div class="col d-flex justify-content-center h-100">
+        <div class="">
+          <h1 class="fw-bolder fs-64">TU COMIDA A SOLO</h1>
+          <h1 class="fw-bolder fs-60">UNOS PASOS</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore porro atque officia,
+            labore repellat perspiciatis rerum similique officiis ipsam dicta saepe explicabo in
+            eaque reprehenderit distinctio praesentium necessitatibus. Ab, debitis!
+          </p>
+
+          <VButton type="secondary">Conoce más</VButton>
+        </div>
+      </div>
+      <div class="col">
+        <h1>Col2</h1>
+      </div>
+    </div>
+  </main>
 </template>
