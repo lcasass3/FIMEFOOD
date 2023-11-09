f768611cc645027b2baf66d92f6aafad5deabe1a
Leon Casas <leonn.casass@gmail.com>
2023-11-09 00:40:13 -0600

    Se configura tailwind
    - Se agregan las variables de colores al tailwind.config
    - Se agrega fuente de letra a styles.css
    - Se agrega color de fondo definido para todo el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgba(68, 165, 112, 1)',
+        secondary: 'rgba(72, 195, 128, 0.6)',
+        font: 'rgba(47, 146, 92, 1)'
+      }
+    },
   },
   plugins: [],
 }
