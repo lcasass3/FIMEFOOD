8ff4b339cb81ee5c86bf2ffb229b26b5cd64d287
Leon Casas <leonn.casass@gmail.com>
2023-11-11 16:02:32 -0600

    Se crea página para seleccionar tipo de cuenta a crear
    - Se crea SelectAccType y carpetas para el flujo de costumer y seller
    - Se realizará el flujo a través de v-if y stage changes para simplificar el router

---
+++ src/pages/SignUp/SelectAccType.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import VButton from '@/components/VButton.vue'
+import CostumerSignUpPage from './costumer/CostumerSignUpPage.vue'
+import SellerSignUpPage from './seller/SellerSignUpPage.vue'
+
+const isCostumer = ref(false)
+const isSeller = ref(false)
+</script>
+
+<template>
+  <main class="flex items-center justify-center h-screen w-screen">
+    <dialog
+      v-if="!isCostumer && !isSeller"
+      open
+      class="bg-white w-1/3 rounded-3xl p-12 flex items-center justify-center max-h-fit max-w-fit shadow-md"
+    >
+      <div>
+        <h1 class="text-2xl font-medium mb-8">Deseo crear una cuenta como:</h1>
+        <VButton type="primary" class="w-full mb-2 px-6" @click="isCostumer = true"
+          >Comprador</VButton
+        >
+        <VButton type="primary" class="w-full mt-2 px-6" @click="isSeller = true">Vendedor</VButton>
+      </div>
+    </dialog>
+
+    <CostumerSignUpPage v-if="isCostumer" />
+    <SellerSignUpPage v-if="isSeller" />
+  </main>
+</template>
---
+++ src/pages/SignUp/seller/SellerSignUpPage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Seller</h1>
+</template>
