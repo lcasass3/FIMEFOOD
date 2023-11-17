40ab2285f3cbbfa64adc8b2799fce11874d1b204
Leon Casas <leonn.casass@gmail.com>
2023-11-16 20:02:06 -0600

    Se crea el archivo routes para mantener el index.ts mas limpio

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { routes } from './routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'landing',
-      component: () => import('../pages/Landing/LandingPage.vue')
-    },
-    {
-      path: '/about-us',
-      name: 'about-us',
-      component: () => import('../pages/Landing/components/AboutUsPage.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../pages/Login/LoginPage.vue')
-    },
-    {
-      path: '/sign-up',
-      name: 'sign-up',
-      component: () => import('../pages/SignUp/SelectAccType.vue')
-    },
-    {
-      path: '/app',
-      name: 'InAppLayout',
-      children: [
-        {
-          path: '/home',
-          name: 'home',
-          component: () => import('../pages/Home/HomePage.vue')
-        },
-        {
-          path: '/favoritos',
-          name: 'favorites',
-          component: () => import('../pages/Home/costumer/components/FavoritesSellersPage.vue')
-        }
-      ],
-      component: () => import('@/layouts/InAppLayout.vue')
-    },
-    
-  ]
+  routes: routes
 })
 
 export default router
---
+++ src/router/routes.ts
@@ -0,0 +1,40 @@
+export const routes = [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('../pages/Landing/LandingPage.vue')
+    },
+    {
+      path: '/about-us',
+      name: 'about-us',
+      component: () => import('../pages/Landing/components/AboutUsPage.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/Login/LoginPage.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'sign-up',
+      component: () => import('../pages/SignUp/SelectAccType.vue')
+    },
+    {
+      path: '/app',
+      name: 'InAppLayout',
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('../pages/Home/HomePage.vue')
+        },
+        {
+          path: '/favoritos',
+          name: 'favorites',
+          component: () => import('../pages/Home/costumer/components/FavoritesSellersPage.vue')
+        }
+      ],
+      component: () => import('@/layouts/InAppLayout.vue')
+    },
+    
+  ]
